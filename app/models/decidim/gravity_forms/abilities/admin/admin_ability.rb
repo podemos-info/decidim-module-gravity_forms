e0517f01faa58ee5e0224fcc9073470d05aa3d9f
David Rodríguez <deivid.rodriguez@riseup.net>
2018-02-14 02:28:13 -0300

    Bring AdminAbility up to date to latest decidim

--- app/models/decidim/gravity_forms/abilities/admin/admin_ability.rb
+++ app/models/decidim/gravity_forms/abilities/admin/admin_ability.rb
@@ -6,28 +6,9 @@ module Decidim
       module Admin
         # Defines the abilities related to gravity_forms for a logged in admin user.
         # Intended to be used with `cancancan`.
-        class AdminAbility
-          include CanCan::Ability
-
-          attr_reader :user, :context
-
-          def initialize(user, context)
-            return unless user && user.role?(:admin)
-
-            @user = user
-            @context = context
-
+        class AdminAbility < Decidim::Abilities::AdminAbility
+          def define_abilities
             can :manage, GravityForm
-          end
-
-          private
-
-          def current_settings
-            context.fetch(:current_settings, nil)
-          end
-
-          def feature_settings
-            context.fetch(:feature_settings, nil)
           end
         end
       end
