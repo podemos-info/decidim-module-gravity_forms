e2d780de99f51b96c3ea37a748a94dd2585c975a
David Rodríguez <deivid.rodriguez@riseup.net>
2018-03-07 09:35:54 -0300

    Add Spanish locales
    And ensure we keep them sane.

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -3,7 +3,7 @@
 require "i18n/tasks"
 
 RSpec.describe "I18n" do
-  let(:i18n) { I18n::Tasks::BaseTask.new }
+  let(:i18n) { I18n::Tasks::BaseTask.new(locales: [:en, :es]) }
   let(:missing_keys) { i18n.missing_keys }
   let(:unused_keys) { i18n.unused_keys }
   let(:non_normalized_paths) { i18n.non_normalized_paths }
